75f6899c112fa53c4b2fda6a6276900b0d4144a6
sefakor20 <sefakorhom2012@gmail.com>
2026-02-02 07:58:53 +0000

    feat: disable user registration feature for invitation-only access

--- config/fortify.php
+++ config/fortify.php
@@ -144,7 +144,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(), // Disabled - users join via invitation only
         Features::resetPasswords(),
         Features::emailVerification(),
         Features::twoFactorAuthentication([
